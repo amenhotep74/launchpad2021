57dd809711e07a59f453f6a0009e808ee4aca90f
amenhotep74 <blewie144@gmail.com>
2021-08-24 11:42:01 +0930

    sendgrid contact form added

--- pages/contact.js
+++ pages/contact.js
@@ -13,6 +13,8 @@ export default function Contact() {
   const [email, setEmail] = useState("");
   const [website, setWebsite] = useState("");
   const [message, setMessage] = useState("");
+  const [error, setError] = useState(false);
+  const [successAlert, setSuccessAlert] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -27,6 +29,17 @@ export default function Contact() {
     fetch("/api/contact", {
       method: "post",
       body: JSON.stringify(data),
+    }).then((response) => {
+      console.log("RESPONSE ITSELF", response);
+      console.log("INSIDE RESPONSE");
+      // if response status 200
+      if (response.status == 200) {
+        // clear errors
+        setError(false);
+        setSuccessAlert(true);
+      } else {
+        setError(true);
+      }
     });
   };
 
@@ -44,9 +57,16 @@ export default function Contact() {
 
         <form className={styles.contact_form} onSubmit={handleSubmit}>
           <div className="mb-3">
-            <div className="alert alert-primary" role="alert">
-              A simple primary alert—check it out!
-            </div>
+            {successAlert && (
+              <div className="alert alert-success" role="alert">
+                👋 Thanks for getting in touch! we will contact you shortly.
+              </div>
+            )}
+            {error && (
+              <div className="alert alert-danger" role="alert">
+                There was an error try again later.
+              </div>
+            )}
 
             <label htmlFor="exampleInputEmail1" className="form-label">
               Name
@@ -58,6 +78,7 @@ export default function Contact() {
               aria-describedby="name"
               placeholder="Enter your name"
               onChange={(e) => setName(e.target.value)}
+              required
             />
           </div>
           <div className="mb-3">
@@ -70,6 +91,7 @@ export default function Contact() {
               id="email"
               placeholder="Enter your email"
               onChange={(e) => setEmail(e.target.value)}
+              required
             />
           </div>
 
@@ -100,6 +122,7 @@ export default function Contact() {
               id="email"
               rows="4"
               onChange={(e) => setMessage(e.target.value)}
+              required
             ></textarea>
           </div>
 
