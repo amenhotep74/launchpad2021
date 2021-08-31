92035fe109ec5a1be6f8680acb5cc3bc0a86e327
amenhotep74 <blewie144@gmail.com>
2021-08-31 16:38:12 +0930

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,27 @@
 import Head from "next/head";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 // Add bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script
-          src="https://kit.fontawesome.com/d60d3e1a74.js"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-}
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+};
 
 export default MyApp;
